1752a7df4ce3528b9dfebf24aad225a8cf38dd2e
Sok4h <mrsokaxd@gmail.com>
2022-01-02 19:00:11 -0500

    bottom nav working

--- app/src/main/java/com/sokah/valorantapp/view/MainActivity.kt
+++ app/src/main/java/com/sokah/valorantapp/view/MainActivity.kt
@@ -2,21 +2,48 @@ package com.sokah.valorantapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.sokah.valorantapp.R
+import com.sokah.valorantapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        navController=this.findNavController(R.id.navHost)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHost)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, nd: NavDestination, _ ->
+            if (nd.id == R.id.agentDetailsFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+            val appBarConfiguration = AppBarConfiguration
+                .Builder(
+                    R.id.agentsFragment,
+                    R.id.weaponListFragment,
+                    )
+                .build()
+            NavigationUI.setupActionBarWithNavController(this, navController,appBarConfiguration)
+
+
+        }
 
 
     }
